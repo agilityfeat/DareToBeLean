c5720517b47dbe2fe3d90facc5a240aae444f75d
Arin Sime <asime@o19s.com>
2012-07-09 04:54:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DareToBeLean::Application.config.secret_token = '973c03d77f28c6aace515b4e8e00e85c34d91276dc3ced8f7cf098d9dfd6bda48b5fceb16b701ae8d84bfe08b4a500edd12ff53e5b5cfae69834cd581491d73c'
